ee138fa106fe9f5024eacace21cfa3c83eaa533e
financeRyu <fb0982@naver.com>
2025-02-19 21:43:51 +0900

    feat : Hello, world 출력하기 2025-02-19 21:50:00

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,11 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="ko">
+      <body>
+        <div className={"w-full min-h-screen"}>
+            {children}
+        </div>
       </body>
     </html>
   );
