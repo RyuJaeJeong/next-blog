0d3093772bee5526d9f1fc7aa4e844d5c454c6bd
financeRyu <fb0982@naver.com>
2025-02-20 22:24:24 +0900

    feat: 사이드바 레이아웃 추가 2025-02-20 22:25:00

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
 import "./globals.css";
+import Navbar from "@/src/components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body>
-        <div className={"w-full min-h-screen"}>
+        <div className={"w-full h-[150VH] min-h-screen"}>
+            <Navbar />
             {children}
         </div>
       </body>
