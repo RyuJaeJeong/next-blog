b7bb000a512504774a63160eecb77ad6c2614a3a
financeRyu <74393467+RyuJaeJeong@users.noreply.github.com>
2025-04-02 00:02:52 +0900

    deploy test final

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className={"flex w-full h-screen items-center"}>
         <div className={"w-full h-auto text-center"}>
-            <p className={"text-6xl font-bold"}>Hello, world?</p>
+            <p className={"text-6xl font-bold"}>Hello, world?!!!!</p>
         </div>
     </div>
   );
