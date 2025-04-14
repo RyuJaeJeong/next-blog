ec19c9fba9a603d9f28b856cb3164f0ffe47360e
financeRyu <fb0982@naver.com>
2025-04-14 20:55:07 +0900

    feat: 기본 레이아웃 세팅 2025-04-14

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,13 @@
-import Navbar from "@/src/components/Navbar";
+
 
 export default function Home() {
   return (
-    <div className={"md:w-2/3 h-[100VH] mx-auto"}>
-        <div className={"w-full h-16"}>
-            <Navbar />
-        </div>
-        <div className={"flex w-full min-h-[90VH] justify-center"}>
-            <div className="flex items-center prose">
-                <h1>
-                    Hello, world!
-                </h1>
-            </div>
-        </div>
-    </div>
+      <div className={"flex w-full min-h-[90VH] justify-center"}>
+          <div className="flex items-center prose">
+              <h1>
+                  Hello, world!
+              </h1>
+          </div>
+      </div>
   );
 }
