7190eb7e474447f78efbc62ca17560ddc6f973cb
financeRyu <fb0982@naver.com>
2025-04-20 21:46:10 +0900

    feat: 게시물 삭제 기능 추가 1. 삭제 기능 구현 2. 삭제 버튼 추가 및 일부 스타일 수정 2025-04-20 류재정

--- src/app/detail/[id]/page.jsx
+++ src/app/detail/[id]/page.jsx
@@ -1,5 +1,7 @@
+import BtnDelete from "@/src/app/detail/[id]/BtnDelete";
 import BtnGoModifyForm from "@/src/app/detail/[id]/BtnGoModifyForm";
 import BtnGoList from "@/src/app/detail/[id]/BtnGoList";
+
 
 const Detail = async (props) => {
     const params = await props.params
@@ -7,7 +9,6 @@ const Detail = async (props) => {
     const API_URL = process.env.NEXT_PUBLIC_API_URL
     const result = await fetch(`${API_URL}/api/articles/${id}`);
     const data = await result.json();
-
 
     return(
         <article className={"w-4/5 md:w-3/5 min-h-[95VH] h-auto mx-auto px-2 py-10 prose max-w-none"}>
@@ -19,10 +20,16 @@ const Detail = async (props) => {
                     류재정, { data.data[0].updDttm }
                 </p>
             </div>
-            <div className={"w-full h-auto my-5 text-xl"} dangerouslySetInnerHTML={{ __html: data.data[0].contents}}></div>
-            <div className={"w-full h-auto my-3 items-end flex justify-end"}>
-                <BtnGoList className={"me-2"} />
-                <BtnGoModifyForm id={id} />
+            <div className={"w-full h-auto my-5 text-xl contents-area"} dangerouslySetInnerHTML={{ __html: data.data[0].contents}}></div>
+            <div className={"w-full h-auto my-3 items-end flex justify-between"}>
+                <div>
+                    <BtnGoModifyForm id={id} />
+                    <BtnDelete className={"ms-2 text-white"} id={id} />
+                </div>
+                <div>
+                    <BtnGoList className={""} />
+                </div>
+
             </div>
         </article>
     )
