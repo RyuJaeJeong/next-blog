63e2b36f75bc241829711d6ad935d9d36b8626cb
financeRyu <fb0982@naver.com>
2025-04-20 20:07:37 +0900

    feat: 수정화면 기능 구현 1. 수정 api 추가 2. 수정 화면 구현 2025-04-20 류재정

--- src/pages/api/articles/index.js
+++ src/pages/api/articles/index.js
@@ -36,5 +36,35 @@ export default async function handler(req, res){
         }finally {
             if (conn) return conn.release();
         }
+    }else if(req.method === 'PATCH'){
+        const data = {};
+        try{
+            var conn = await pool.getConnection();
+            const param = JSON.parse(req.body)
+
+            const result = await conn.query({
+                sql: 'UPDATE t_board T1 SET T1.title=?, T1.contents=?, T1.upd_dttm=now() WHERE T1.id=?'
+            }, [param.title, param.contents, param.id]);
+
+            if(result[0].affectedRows > 0){
+                data.code = 1;
+                data.message = "처리성공";
+            }else{
+                data.code = 0;
+                data.message = "영향을 끼치지 않았습니다";
+            }
+
+            return res.status(200).json(data);
+        }catch (e) {
+            console.log(e);
+
+            data.code = -1;
+            data.message = "관리자에게 문의하세요.";
+            data.data = e.toString();
+
+            return res.status(500).json(data)
+        }finally {
+            if (conn) return conn.release();
+        }
     }
 }
