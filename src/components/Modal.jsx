aceba6775e31d90a4fa148a61728766892785f35
financeRyu <fb0982@naver.com>
2025-04-20 20:10:35 +0900

    feat: 모달 컴포넌트 기능 추가 1. close 버튼 포함 여부 기능 추가 2025-04-20 류재정

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 const Modal = (props) => {
+    const isClose = props.isClose;
     return (
         <dialog id="my_modal_1" className="modal">
             <div className="modal-box">
@@ -8,10 +9,10 @@ const Modal = (props) => {
                 <p className="py-4"> { props.modalContents } </p>
                 <div className="modal-action">
                     <form method="dialog">
-                        <button className="btn btn-neutral mx-2" onClick={()=>{props.btnFunction()}}>
+                        <button className={`btn btn-neutral ${isClose?"mx-2":""}`} onClick={()=>{props.btnFunction()}}>
                             { props.btnText }
                         </button>
-                        <button className="btn">Close</button>
+                        <button className={`btn ${isClose?"":"hidden"}`}>Close</button>
                     </form>
                 </div>
             </div>
