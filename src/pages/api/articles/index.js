1cd2dd7943abaa74a22d00cf2204d250820e8a3e
financeRyu <fb0982@naver.com>
2025-05-13 22:30:35 +0900

    feat: 페이지 데이터 계산기능 추가 1. 게시물 총 갯수 관련 함수 2. 페이저 데이터 계산 관련 함수 추가 3. API Data 추가 2025-04-25 류재정

--- src/pages/api/articles/index.js
+++ src/pages/api/articles/index.js
@@ -13,15 +13,18 @@ export default async function handler(req, res){
                              " WHERE 1 = 1"  +
                                " AND T1.delete_yn = 0 ";
             const pageNo = req.query.pageNo || 1;
-            const contentsSize = 13;
-            let stNum = (pageNo - 1) * contentsSize;
+            const pageSize = 15;
+            const totalRecord = await getTotalRecord();
+            const pageData = pager(pageSize, 10, totalRecord, pageNo);
+
             sql += "LIMIT ?, ?"
             console.log(sql)
-            const rows = await conn.query(sql, [stNum, contentsSize]);
-            console.log(rows)
+            const rows = await conn.query(sql, [pageData.startRecord, pageSize]);
+
             data.code = 200;
             data.message = "처리성공";
             data.data = rows[0];
+            data.pageData = pageData;
         }catch (e) {
             console.error(e)
             data.code = 500;
@@ -97,3 +100,79 @@ export default async function handler(req, res){
         }
     }
 }
+
+
+
+
+function pager(pageSize, blockSize, totalRecord, pageNo){
+    const startRecord = pageSize * (pageNo - 1);
+    let lastRecord = pageSize * pageNo;
+    if(lastRecord > totalRecord) lastRecord = totalRecord
+    let totalPage = 0;
+    let startPage = 1;
+    let lastPage = 1;
+    if(totalRecord > 0){
+        totalPage = totalRecord / pageSize + (totalRecord % pageSize == 0 ? 0 : 1)
+        startPage = (pageNo / blockSize - (pageNo % blockSize != 0 ? 0 : 1)) * blockSize + 1;
+        lastPage = startPage + blockSize - 1
+        if(lastPage > totalPage) lastPage = totalPage
+    }
+    return {
+        "startRecord":startRecord,
+        "lastRecord":lastRecord,
+        "totalPage":Math.ceil(totalPage),
+        "startPage":startPage,
+        "lastPage":lastPage
+    }
+}
+
+async function getTotalRecord(){
+    try{
+        var conn = await pool.getConnection();
+        let sql = "SELECT COUNT(1) AS cnt " +
+                           "FROM t_board T1" +
+                         " WHERE 1=1"  +
+                           " AND T1.delete_yn = 0 ";
+        console.log(sql)
+        const rows = await conn.query(sql);
+        console.log(rows)
+        return rows[0][0].cnt
+    }catch (e) {
+      return 0
+    }
+}
+
+
+
+
+// public int[] pager(int pageSize, int blockSize, int totalRecord, int pageNumber) {
+//     int[]  result = new int[6];
+//     int jj = totalRecord - pageSize * (pageNumber - 1);
+//     int startRecord = pageSize*(pageNumber-1)+1;
+//     int lastRecord = pageSize * pageNumber;
+//     if( lastRecord >totalRecord) {
+//         lastRecord = totalRecord;
+//     }
+//
+//     int totalPage = 0;
+//     int startPage = 1;
+//     int lastPage = 1;
+//     if(totalRecord > 0) {
+//         totalPage = totalRecord / pageSize + (totalRecord % pageSize == 0? 0:1);
+//         startPage = (pageNumber / blockSize - (pageNumber % blockSize != 0? 0:1)) * blockSize + 1;
+//         lastPage = startPage + blockSize - 1;
+//         if (lastPage>totalPage) {
+//             lastPage = totalPage;
+//         }
+//     }
+//
+//     result[0] = jj;
+//     result[1] = startRecord;
+//     result[2] = lastRecord;
+//     result[3] = totalPage;
+//     result[4] = startPage;
+//     result[5] = lastPage;
+//
+//     return result;
+//
+// }
