cf9bacaef9a312831e34d9cfe318186267d6c0f0
financeRyu <fb0982@naver.com>
2025-07-17 22:36:35 +0900

    first commit

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:18.20.4-slim
-WORKDIR /next-blog
-COPY . .
-RUN ["npm", "install"]
-RUN ["npm", "run", "build"]
-EXPOSE 3000
-ENTRYPOINT ["npm", "start"]
-LABEL authors="financeRyu"
-#CMD ["npm", "start"]
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="footer sm:footer-horizontal footer-center bg-[#F6F7F8] text-base-content p-4">
-            <aside>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved by financeRyu</p>
-            </aside>
-        </footer>
-    )
-}
