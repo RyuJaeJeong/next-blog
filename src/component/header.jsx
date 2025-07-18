496deef0504127ebefab1676df3521ac7b5b3655
financeRyu <fb0982@naver.com>
2025-07-18 21:33:08 +0900

    feat: layout 구현 1. header 컴포넌트 추가 2. header 가변 인자 파라미터 화 3. footer 배치 2025-07-18 류재정

---
+++ src/component/header.jsx
@@ -0,0 +1,52 @@
+import Link from 'next/link'
+
+const Header = (props) => {
+    return(
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
+                <div className="container px-4 px-lg-5">
+                    <Link className="navbar-brand" href="/">Start Bootstrap</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        <i className="fas fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ms-auto py-4 py-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/article">Sample Post</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <header className="masthead" style={{backgroundImage: `url(${props.image})`}}>
+                <div className="container position-relative px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-md-10 col-lg-8 col-xl-7">
+                            <div className="site-heading">
+                                <h1 className={`${(props.head)?"":"d-none"}`}>
+                                    {props.head}
+                                </h1>
+                                <h2 className={`subheading ${(props.subhead)?"":"d-none"}`}>
+                                    {props.subhead}
+                                </h2>
+                                <span className={`meta ${(props.meta)?"":"d-none"}`}>
+                                    {props.meta}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+export default Header
