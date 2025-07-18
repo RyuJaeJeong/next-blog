1b4c6c6da2832dea687efc1f49c20189a960c91e
financeRyu <fb0982@naver.com>
2025-07-18 22:36:46 +0900

    feat: sample article 구현 1. form page추가 2. 기타 블라블라수정 2025-07-18 류재정

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -5,7 +5,7 @@ const Header = (props) => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
                 <div className="container px-4 px-lg-5">
-                    <Link className="navbar-brand" href="/">Start Bootstrap</Link>
+                    <Link className="navbar-brand" href="/">Next.log()</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         <i className="fas fa-bars"></i>
                     </button>
@@ -15,13 +15,13 @@ const Header = (props) => {
                                 <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">About</Link>
+                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/article">List</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/article">Sample Post</Link>
+                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/article/1">Sample Post</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">Contact</Link>
+                                <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/article/form">Form</Link>
                             </li>
                         </ul>
                     </div>
