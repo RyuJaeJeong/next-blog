28fdde531810c702abdc5b9d2bfa2efd592505b3
financeRyu <fb0982@naver.com>
2025-07-26 16:33:59 +0900

    fix: GET 메서드 이외의 메서드에 대한 리턴 문 추가 2025-07-26 류재정

--- pages/api/test.jsx
+++ pages/api/test.jsx
@@ -5,6 +5,8 @@ const handler = async (req, res) => {
         const sql = neon(`${process.env.DATABASE_URL}`);
         const rows = await sql.query(`SELECT TO_CHAR(NOW() AT TIME ZONE 'Asia/Seoul', $1) AS now`, ['YYYY-MM-DD HH24:MI:SS']);
         return res.status(200).json({"now" : rows[0]})
+    }else{
+        return res.status(404).json({"msg" : "not found"})
     }
 }
 
