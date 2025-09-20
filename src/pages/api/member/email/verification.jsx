a54e0343355e5304355a2d05ee4ab5c405cad238
financeRyu <fb0982@naver.com>
2025-09-20 22:25:50 +0900

    feat: nodemailer를 활용한 mail발송 기능 추가 2025-09-20 류재정

--- src/pages/api/member/email/verification.jsx
+++ src/pages/api/member/email/verification.jsx
@@ -1,4 +1,18 @@
+import nodemailer from 'nodemailer';
 import { pool, mybatisMapper } from "@/utils/db";
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth:{
+        type: "OAuth2",
+        user: "ryoojj8998@gmail.com",
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_CLIENT_TOKEN,
+    }
+});
 
 const handler = async(req, res) => {
     if(req.method == 'GET'){
@@ -9,6 +23,13 @@ const handler = async(req, res) => {
             console.log(sql)
             const { rows } = await conn.query(sql);
             console.log(rows)
+            await transporter.sendMail({
+                from: "ryoojj8998@gmail.com",
+                to: rows[0].email,
+                subject: "Next.log()에서 이메일 인증번호가 도착하였습니다",
+                text: `인증번호: ${rows[0].verification_code}`,
+            });
+
             return res.status(200).json({
                 code: 200,
                 msg: "success",
