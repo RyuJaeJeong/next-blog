7b410d3b455caacbc771193be15e90cb0e97db03
financeRyu <fb0982@naver.com>
2025-11-03 21:44:00 +0900

    feat: 인증코드 발급, 회원 가입, 회원 삭제 테스트 추가 2025-11-03 류재정

--- test/pages/member.test.js
+++ test/pages/member.test.js
@@ -1,5 +1,5 @@
-import handler from "@/pages/api/member/email/verification";
-// import handler from "@/pages/api/member";
+import verificationHandler from "@/pages/api/member/email/verification";
+import memberHandler from "@/pages/api/member";
 import { createMocks } from 'node-mocks-http';
 import dotenv from 'dotenv'
 
@@ -7,32 +7,41 @@ describe('/api/member', ()=>{
     beforeEach(()=>{
         return dotenv.config({ path: '.env.local' });
     });
-
     let verificationCode;
     test('이메일 인증코드 발행', async ()=>{
         const {req, res} = createMocks({
             method: 'GET',
             query: {
-                email: 'test210@example.com'
+                email: 'test235@example.com'
             }
         });
-        await handler(req, res);
-        verificationCode = JSON.parse(res._getData()).data.verification_code;
+        await verificationHandler(req, res);
+        verificationCode = JSON.parse(res._getData()).data.verificationCode;
         expect(res._getStatusCode()).toBe(200);
     }, 10000);
-
     test('회원가입 테스트', async ()=>{
+        console.log(`verificationCode2: ${verificationCode}`);
         const {req, res} = createMocks({
             method: 'POST',
             body: JSON.stringify({
                 name: "류재정",
-                email: "test210@example.com",
+                email: "test235@example.com",
                 verificationCode: verificationCode,
                 password: "123",
                 passwordCheck: "123"
             })
         });
-        await handler(req, res);
-        expect(res._getStatusCode()).toBe(200);
+        await memberHandler(req, res);
+        expect(JSON.parse(res._getData()).data).toBe(1);
+    });
+    test('회원삭제 테스트', async ()=>{
+        const {req, res} = createMocks({
+            method: 'DELETE',
+            query: {
+                email: "test235@example.com",
+            }
+        });
+        await memberHandler(req, res);
+        expect(JSON.parse(res._getData()).data).toBe(1);
     });
 });
