acb442efcd68c2556e96d61e12f02158b19619cd
financeRyu <fb0982@naver.com>
2025-12-06 21:39:49 +0900

    feat: 게시물 입력 api 추가 2025-12-06 류재정

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -90,6 +90,7 @@ export const authOptions = {
         jwt: async ({ token, user }) => {
             if (user) {
                 token.user = {};
+                token.user.id = user.id
                 token.user.name = user.name
                 token.user.email = user.email
             }
