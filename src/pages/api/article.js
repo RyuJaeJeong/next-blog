d0aef08d37ff0ea3b992054c77f5c05360b04b71
financeRyu <fb0982@naver.com>
2025-12-06 21:39:36 +0900

    feat: 게시물 입력 api 추가 2025-12-06 류재정

---
+++ src/pages/api/article.js
@@ -0,0 +1,35 @@
+import { pool, mybatisMapper } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+const handler = async(req, res)=>{
+    if(req.method == 'POST'){
+        try{
+            var conn = await pool.connect();
+            const param = JSON.parse(req.body);
+            const session = await getServerSession(req, res, authOptions);
+            const userId = session.user.id;
+            param.userId = userId;
+            const sql = mybatisMapper.getStatement("articleMapper", "insertArticle", param);
+            console.log(sql);
+            const result = await conn.query(sql);
+            console.log(result);
+            return res.status(200).json({
+                code: 200,
+                msg: "success",
+                data: result.rowCount
+            });
+        }catch (e) {
+            console.error("EEEEEEEEEEEEEEEE");
+            console.error(e);
+            return res.status(500).json({
+                code: 500,
+                msg: e.message
+            })
+        }finally {
+            if(conn) conn.release();
+        }
+    }
+}
+
+export default handler
